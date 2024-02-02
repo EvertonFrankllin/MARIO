c2a7442ec8166cfdabfa42c6914df203e9f751a1
EvertonFrankllin <evertonfrankllintrc@gmail.com>
2024-02-02 03:33:44 -0300

    Versão 2.0

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,12 @@
+//Acessando Documentação do HTML
+const mario = document.querySelector('.MARIO');//class mario
+const pipe = document.querySelector('.pipe');//class pipe
 
-const mario = document.querySelector('.MARIO');
-const pipe = document.querySelector('.pipe');
-
-const botao = document.querySelector('.botao');
+const botao = document.querySelector('.botao');//função de jumper com click
+const cloud = document.querySelector('.cloud ');//class nuvem
+const cloud2 = document.querySelector('.cloud2 ');//class nuvem
+const cloud3 = document.querySelector('.cloud3 ');//class nuvem
+const cloud4 = document.querySelector('.cloud4 ');//class nuvem
 
 const jump = ()=>{
     mario.classList.add('jump', 'botao');
@@ -23,6 +27,11 @@ const loop = setInterval(() => {
     if(pipeposition<=138 && pipeposition > 0 && marioposition < 80){
         pipe.style.animation= 'none';
         pipe.style.left = `${pipeposition}px`; 
+
+        cloud.style.animation = 'none';
+        cloud2.style.animation = 'none';
+        cloud3.style.animation = 'none';
+        cloud4.style.animation = 'none';
 
         mario.style.animation= 'none';
         mario.style.bottom = `${marioposition}px`; 
